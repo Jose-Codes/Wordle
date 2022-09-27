cede8f0e5a60a2c278d735bbd5e7479c48bf4493
Jose-Codes <josepujol21@gmail.com>
2022-09-27 11:45:26 -0400

    Text Error handling and Game Manager class

---
+++ Assets/GameManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Purpose: This class 
+/// </summary>
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// Assings the colors to the characters
+    /// depending on their match.
+    /// JAZZY
+    /// FUZZY
+    /// return: [-1, -1, 1, 1, 1]
+    /// -1 --> Red
+    /// 0 --> Yellow
+    /// 1 --> Green
+    /// </summary>
+    int[] checkMatches()
+    {
+        // TO DO:
+        // Implement colors
+        int[] colors = new int[5];
+        return colors;
+    }
+}
--- Assets/ReadPlayerText.cs
+++ Assets/ReadPlayerText.cs
@@ -2,24 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class ReadPlayerText : MonoBehaviour
 {
-
-    public Text getText;
+    private GameManager GM;
+    public TextMeshProUGUI getText;
 
     // Start is called before the first frame update
     void Start()
     {
-        getText = GetComponent<Text>();    
+
     }
 
     // Update is called once per frame
     void Update()
     {
         foreach(char c in Input.inputString) 
-        { 
-            if (c == '\b') // has backspace/delete been pressed?
+        {
+            if (getText.text.Length == 5) break;
+
+            else if (c == '\b') // has backspace/delete been pressed?
             {
                 if (getText.text.Length != 0)
                 {
@@ -32,8 +35,25 @@ public class ReadPlayerText : MonoBehaviour
             }
             else
             {
-                getText.text += c;
+                // check valid
+                if (validInput(c))
+                    getText.text += System.Char.ToUpper(c);
+                else
+                    Debug.Log("Invalid character: " + (int)c);
             }
         }
+    }
+
+
+    /*
+     Errors:
+     1) Must be ASCII values 65-90 or 97-122
+     */
+    bool validInput(char c)
+    {
+        if ((65 <= (int)c && (int)c <= 90) || (97 <= (int)c && (int)c <= 122))
+            return true;
+
+        return false;
     }
 }
